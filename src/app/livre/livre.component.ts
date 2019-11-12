ca97a5c537726684a4b73b00e9d77bf53f4cd7ec
mohamedchebil <mohamed.chebil15@gmail.com>
2019-11-12 11:24:23 +0100

    test

---
+++ src/app/livre/livre.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-livre',
+  templateUrl: './livre.component.html',
+  styleUrls: ['./livre.component.css']
+})
+export class LivreComponent implements OnInit {
+
+  constructor() { }
+  calcul(){
+    var prix = promo.value*this.prix;
+  }
+  ngOnInit() {
+  }
+
+}
